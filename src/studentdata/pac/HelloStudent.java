2fd12a6c5ed58e6c4730929eea915c714a6b8b48
IMU <IMU@DESKTOP-FSRQE0S>
2023-03-29 15:30:02 +0630

    Second Commit

---
+++ src/studentdata/pac/HelloStudent.java
@@ -0,0 +1,10 @@
+package studentdata.pac;
+
+public class HelloStudent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello IMU Students");
+	}
+
+}
